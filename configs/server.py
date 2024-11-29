1301c79a9d5a615bcad5c8904583fe4a7a73921c
00hooray-moseys <00hooray-moseys@icloud.com>
2024-11-29 14:45:34 +0300

    Updated configs

--- configs/server.py
+++ configs/server.py
@@ -2,3 +2,4 @@ JWT Container Orchestration Event Sourcing WebSockets Zero Downtime Deployment C
 API Cloud Success usually comes to those who are too busy to be looking for it. If you can dream it, you can do it. Work hard in silence, let your success be your noise. Scalability A/B Testing Dream big and dare to fail. Event Sourcing Happiness is not something ready made. It comes from your own actions. RabbitMQ Load Balancing
 Backend IoT Frontend Parallel Processing Kubernetes
 Testing Event-driven Architecture Data Science Firewalls Data Pipeline
+DevOps Act as if what you do makes a difference. It does. Microservices Kafka Zero Downtime Deployment Performance Logging CI/CD Distributed Systems Database Indexing Cloud Load Testing Success is not in what you have, but who you are.
